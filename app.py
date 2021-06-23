932541dc953daf9ce1eb3a7d081b3c1d204fcc8b
Meng-Chieh, Liu <77425545+MengChiehLiu@users.noreply.github.com>
2021-06-24 00:47:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 @app.route("/home") #根目錄
 def test():
-    return render_template("cover.html")
+    return "Hello"
 
 @app.route("/forms", methods=['GET']) #根目錄
 def forms():
